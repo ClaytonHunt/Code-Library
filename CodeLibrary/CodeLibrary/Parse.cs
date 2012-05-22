2698e5175db5d3cb982a84a74133783d2a79adc1
Clayton Hunt <claytonhunt_104@hotmail.com>
2012-05-21 20:27:23 -0400

    Added Parse Class
    This class should parse any base type from string or convertable type

---
+++ CodeLibrary/CodeLibrary/Parse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeLibrary
+{
+    public class Parse : SingletonBase<Parse>
+    {
+        private Parse() { }
+
+        public T To<T>(object value) { return To<T>(value, default(T)); }
+
+        public T To<T>(object value, T failValue)
+        {
+            try { return (T)Convert.ChangeType(value, typeof(T)); }
+            catch { return failValue; }
+        }
+    }
+
+    public static class ParseExtenstions
+    {
+        public static T To<T>(this object value)
+        {
+            return Parse.Instance.To<T>(value);
+        }
+
+        public static T To<T>(this object value, T failValue)
+        {
+            return Parse.Instance.To<T>(value, failValue);
+        }
+    }
+}
---
+++ CodeLibrary/LibraryTests/ParseTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeLibrary;
+
+namespace LibraryTests
+{
+    [TestClass]
+    public class ParseTests
+    {
+        [TestMethod]
+        public void Given_ParseToInt_When_BadValue_Then_ReturnDefaultInt()
+        {
+            // Arrange
+            var badValue = "BadValue";
+            var expected = default(int);
+
+            // Act
+            var actual = Parse.Instance.To<int>(badValue);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Given_ParseToInt_When_BadValueAndDefaultValue_Then_ReturnDefaultValue()
+        {
+            // Arrange
+            var badValue = "BadValue";
+            var expected = -1;
+
+            // Act
+            var actual = Parse.Instance.To<int>(badValue, expected);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Given_ParseToInt_When_GoodValue_Then_ReturnIntValue()
+        {
+            // Arrange
+            var goodValue = "1";
+            var expected = 1;
+
+            // Act
+            var actual = Parse.Instance.To<int>(goodValue);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Given_ToInt_When_BadValue_Then_ReturnDefaultInt()
+        {
+            // Arranage
+            var badValue = "BadValue";
+            var expected = default(int);
+
+            // Act
+            var actual = badValue.To<int>();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Given_ToInt_When_BadValueAndDefaultValue_Then_ReturnDefaultValue()
+        {
+            // Arranage
+            var badValue = "BadValue";
+            var expected = -1;
+
+            // Act
+            var actual = badValue.To<int>(expected);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
